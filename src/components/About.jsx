9a5055d79cc8ed532787cd67a0f20f90f638a72d
Olive Ade <ade_o@etna-alternance.net>
2023-03-22 12:13:08 +0100

    Portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
                 first-letter:mr-3 first-letter:float-left text-justify'>Je suis fascinée et intéressée par tous ce qui a relation avec le domaine des sciences  généralement et l’informatique particulièrement.
                  Ma passion pour l'informatique a commencé très tôt dans mon parcours scolaire. J'ai toujours été fasciné par la façon dont les ordinateurs et les technologies numériques peuvent résoudre des problèmes complexes, 
                  rendre la vie plus facile et apporter des changements significatifs dans le monde. Mon objectif est donc d’acquérir un solide socle de connaissances et de développer des compétences techniques et méthodologiques dans tous les 
-                domaines de l’informatique à fin d’exercer le métier de mon rêve autant qu’une une développeuse web comme il faut. Et effectivement obtenir un diplôme reconnu et en adéquation avec mon projet professionnel.</p>
+                domaines de l’informatique afin d’exercer le métier de mon rêve autant qu’une une développeuse web comme il faut. Et effectivement obtenir un diplôme reconnu et en adéquation avec mon projet professionnel.</p>
             </div>
             <div className=' p-20 my-20'>
                 <img className='w-8/12 h-8/12' src={c} class="hidden md:block w-8/12 h-4/12" alt="/" />
