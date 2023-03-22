3a473e20654d62312f86dd5ebb0b2a35fd562434
Olive Ade <ade_o@etna-alternance.net>
2023-03-22 12:12:11 +0100

    Portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,6 @@ module.exports = {
   theme: {
   
     extend: {},
-      screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '600px',
-      'xl': '600px',
-      '2xl': '600px',
-
-    }
   },
   plugins: [],
 }
