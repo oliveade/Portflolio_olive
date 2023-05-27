af21283782e33cbeac2af6e342667de14a271417
Olive Ade <ade_o@etna-alternance.net>
2023-05-27 09:42:23 +0200

    portfolio

--- src/components/Cursus.jsx
+++ src/components/Cursus.jsx
@@ -12,7 +12,8 @@ const Cursus = () => {
           <h2 className='text-3xl uppercase'>Formations</h2>
         </div>
         <div className='grid md:grid-cols-2 gap-4'>
-        <a href="# " className='flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
+        
+        <a href="# " className='transition-opacity flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
         
         <img className="object-cover w-full rounded-t-lg h-90 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={cImg} alt=""/>
         <div className="flex flex-col justify-between p-4 leading-normal">
@@ -49,6 +50,6 @@ const Cursus = () => {
       </div>
     </div>
   );
-};
 
+};
 export default Cursus;
